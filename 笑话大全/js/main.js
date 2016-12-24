7e494d2c1f1edb5c6493149b94e8b697716c5a66
WangXingWeb <wx1620@163.com>
2016-12-24 21:16:04 +0800

    2016.12

---
+++ 笑话大全/js/main.js
@@ -0,0 +1,57 @@
+var container=$("#myContainer");
+var index=1;
+function formatterDateTime() {
+    var date=new Date()
+    var month=date.getMonth() + 1
+    var datetime = date.getFullYear()
+        + ""// "年"
+        + (month >= 10 ? month : "0"+ month)
+        + ""// "月"
+        + (date.getDate() < 10 ? "0" + date.getDate() : date
+            .getDate())
+        + ""
+        + (date.getHours() < 10 ? "0" + date.getHours() : date
+            .getHours())
+        + ""
+        + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date
+            .getMinutes())
+        + ""
+        + (date.getSeconds() < 10 ? "0" + date.getSeconds() : date
+            .getSeconds());
+    return datetime;
+}
+function http(index) {
+    $.ajax({
+        type: 'post',
+        url: 'http://route.showapi.com/341-3',
+        dataType: 'jsonp',
+        data: {
+            "showapi_timestamp": formatterDateTime(), //注意要使用当前时间。服务器只处理时间误差10分钟以内的请求
+            "showapi_appid": '29541', //这里需要改成自己的appid
+            "showapi_sign": '3fd121b3d11845d3bddd36c8e2a6bd6a',  //这里需要改成自己的密钥
+            "page":index,
+            "maxResult":'10',
+        },
+        jsonp: 'jsonpcallback',
+        error: function(XmlHttpRequest, textStatus, errorThrown) {
+            alert("操作失败!");
+        },
+        success: function(result) {
+            console.log(result);//console变量在ie低版本下不能用
+            var arry=result.showapi_res_body.contentlist;
+            for(var i=0;i<arry.length;i++){
+                container.append('<div class="content"><img src="'+arry[i].img+'"/><h5 class="img-title">'+arry[i].title+'</h5><hr class="divider"/> ');
+            }
+            console.log(index);
+        }
+    });
+}
+window.onload=function(){
+    http(index);
+    index++;
+    document.getElementById('btn-more').onclick=function(){
+        http(index);
+        index++;
+    }
+}
+
